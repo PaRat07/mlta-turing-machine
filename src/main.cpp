ff9c5f9622dc22426579b44bfd168822ac3e8156
PaRat07 <pasha.at.phone@gmail.com>
2026-04-21 03:09:52 +0300

    complete execution pipeline

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
+#include <config.hpp>
+#include <parser.hpp>
+#include <interpreter.hpp>
+
 #include <iostream>
 
-int main() {
-  std::cout << "Hello World!" << std::endl;
+int main(int argc, const char* argv[]) {
+  if (argc < 2) {
+    throw std::invalid_argument("Wrong number of cli arguments");
+  }
+  std::string_view config_path = argv[1];
+  auto config = ParseConfig(config_path);
+
+  Interpreter interpreter(std::move(config));
+  while (interpreter.GetState() == Interpreter::State::kRunning) {
+    interpreter.PrintState(std::cout);
+    interpreter.Step();
+  }
+  interpreter.PrintState(std::cout);
 }
 
